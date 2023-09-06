199048cd7db5c608f1c19c5c1228a4ce6100bef5
Adrian Hernandez <danix08@live.com>
2023-09-06 13:34:30 -0300

    Terminando seccion resumen

--- src/js/app.js
+++ src/js/app.js
@@ -250,12 +250,59 @@ function mostrarResumen(){
 
     const resumen = document.querySelector('.contenido-resumen');
 
+    
+
+    //Limpiar el contenido Reusmen
+
+    while(resumen.firstChild){
+      resumen.removeChild(resumen.firstChild);
+    }
+
     if(Object.values(cita).includes("") || cita.servicios.length === 0){
 
-      mostrarAlerta('Faltan datos de Servicios, Fecha u Hora','error','.contenido-resumen', desaparece = false)
-    }else{
-      console.log('Todo bien')
+      mostrarAlerta('Faltan datos de Servicios, Fecha u Hora','error','.contenido-resumen', desaparece = false);
+
+      return;
     }
+
+    //Formatear el DIV de resumen
+
+    const {nombre,fecha,hora,servicios} = cita;
+
+    const nombreCliente = document.createElement('P');
+    nombreCliente.innerHTML = `<span>Nombre:</span> ${nombre}`
+
+    const fechaCita = document.createElement('P');
+    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`
+
+    const horaCita = document.createElement('P');
+    horaCita.innerHTML = `<span>Hora:</span> ${hora}`
+
+    servicios.forEach(servicio => {
+      const  {id,precio,nombre} = servicio;
+      const contenedorServicio = document.createElement('DIV');
+      contenedorServicio.classList.add('contenedor-servicio');
+
+      const textoServicio = document.createElement('P');
+      textoServicio.textContent = nombre;
+
+      const precioServicio = document.createElement('P');
+      precioServicio.innerHTML = `<span>Precio:</span> $${precio}`
+
+      contenedorServicio.appendChild(textoServicio)
+      contenedorServicio.appendChild(precioServicio)
+
+      resumen.appendChild(contenedorServicio)
+
+      
+    })
+
+
+    resumen.appendChild(nombreCliente);
+    resumen.appendChild(fechaCita);
+    resumen.appendChild(horaCita);
+   
+
 
 }
 
